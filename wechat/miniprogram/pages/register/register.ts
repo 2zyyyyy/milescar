fb4b1ebd0d95943e3e40525ea0ddad6233d1dd4e
gilbert <2zyyyyy4dev@gmail.com>
2022-07-25 22:59:20 +0800

    add:驾照选择后页面回显

--- wechat/miniprogram/pages/register/register.ts
+++ wechat/miniprogram/pages/register/register.ts
@@ -1,3 +1,16 @@
 Page({
-    
+    data: {
+        licImgUrl: undefined as string | undefined,
+    },
+    onUploadImg() {
+        wx.chooseImage({
+            success: res => {
+                if (res.tempFilePaths.length > 0) {
+                    this.setData({
+                        licImgUrl: res.tempFilePaths[0]
+                    })
+                }
+            }
+        })
+    }
 })
