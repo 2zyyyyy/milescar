9f7d44d8538d3fa4c9dfc14bab5657236b73fc43
gilbert <2zyyyyy4dev@gmail.com>
2022-07-30 23:21:33 +0800

    add:完成驾照上传页面

--- wechat/miniprogram/pages/register/register.ts
+++ wechat/miniprogram/pages/register/register.ts
@@ -1,6 +1,13 @@
 Page({
     data: {
-        licImgUrl: undefined as string | undefined,
+        genders: ['保密', '男', '女'],
+        gender: 0,
+        licNo: '',
+        licName: '',
+        birthDate: '1990-01-01',
+        licImgUrl: '',
+        // 0 未审核  10 审核中  20 审核结束
+        status: 0 as 0 | 10 | 20
     },
     onUploadImg() {
         wx.chooseImage({
@@ -9,8 +16,49 @@ Page({
                     this.setData({
                         licImgUrl: res.tempFilePaths[0]
                     })
+                    // TODO: uploac image
+                    setTimeout(() => {
+                        this.setData({
+                            licNo: '8559488213',
+                            licName: '万里',
+                            gender: 1,
+                            birthDate: '1995-05-25',
+                        })
+                    }, 500);
                 }
             }
         })
-    }
+    },
+    onGenderChange(e: any) {
+        this.setData({
+            gender: e.detail.value,
+        })
+    },
+    onBirthChange(e: any) {
+        this.setData({
+            birthDate: e.detail.value,
+        })
+    },
+    submit() {
+        this.setData({
+            status: 10,
+        })
+        setTimeout(() => {
+            this.verification()
+        }, 1500)
+    },
+    reSubmit() {
+        this.setData({
+            licImgUrl: '',
+            status: 0,
+        })
+    },
+    verification() {
+        this.setData({
+            status: 20
+        })
+        wx.redirectTo({
+            url: '/pages/lock/lock',
+        })
+    },
 })
