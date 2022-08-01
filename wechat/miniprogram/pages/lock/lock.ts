1d2488cde1225c4df0c58f130019dcbdfd9ec793
wanli <zhangyun@ifugle.com>
2022-08-01 11:26:29 +0800

    add:获取用户头像并显示

--- wechat/miniprogram/pages/lock/lock.ts
+++ wechat/miniprogram/pages/lock/lock.ts
@@ -1,12 +1,21 @@
 Page({
     data: {
-        avatorUrl: '',
+        avatarUrl: '',
+        height: 0,
+        width: 0,
     },
     onChooseAvatar(e: any) {
         const {avatarUrl} = e.detail
         this.setData({
             avatarUrl: avatarUrl,
         })
-        console.log("set data success!")
-    }
+        console.log("avatarUrl：", avatarUrl)
+    },
+    load(e: any) {
+        this.setData({
+            height: e.detail.height,
+            width: e.detail.width
+        })
+        console.log("height:", this.data.height, "width:", this.data.height)
+    },
 }) 
